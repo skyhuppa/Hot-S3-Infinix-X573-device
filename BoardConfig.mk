7d659dc50f58690938988fd487549d49a352d3ac
Adasi_Kaakyire <64362452+skyhuppa@users.noreply.github.com>
2021-06-09 22:31:48 +0000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,7 +75,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 #TARGET_KERNEL_CONFIG := X573_defconfig
 
 # Ramdisk compression
-#LZMA_RAMDISK_TARGETS := recovery
+LZMA_RAMDISK_TARGETS := recovery
 
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
